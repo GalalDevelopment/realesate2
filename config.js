3404cf86415f079a9bb81cc48bae918c074d53ca
Galal <adamahmedgalal0@gmail.com>
2026-08-24 02:54:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 //  وسيب باقي الملف زي ما هو.
 // ================================================================
 window.SUPABASE_CONFIG = {
-  url:     "ضع_رابط_المشروع_هنا",   // مثال: https://abcdefgh.supabase.co
-  anonKey: "ضع_مفتاح_anon_هنا",      // المفتاح الطويل المسمى anon public
+  url:     "https://etwvyfdixouvvnvbdkar.supabase.co/",   // مثال: https://abcdefgh.supabase.co
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0d3Z5ZmRpeG91dnZudmJka2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1MjIzNjAsImV4cCI6MjEwMzA5ODM2MH0.PS-rCyA6hDDZt7Y1oO2oaDForw4hacZYyMuX5Q2hJAk",      // المفتاح الطويل المسمى anon public
 };
